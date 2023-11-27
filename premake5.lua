36decbfdcc40767d6fd46b1f3176b92182b0df61
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-11-27 02:27:24 -0300

    refact: turn project into premake5 project

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+workspace "CTechnical"
+    configurations {
+        "Debug",
+        "Release"
+    }
+
+project "CTechnical"
+    kind "ConsoleApp"
+    language "C"
+    cdialect "C17"
+    targetdir "bin/%{cfg.buildcfg}"
+    toolset "clang"
+    includedirs { "packages"}
+
+    files {
+        "src/**.c",
+        "packages/**.c",
+        "packages/**.h"
+    }
+
+    filter "configurations:Debug"
+        defines { "DEBUG"}
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG"}
+        optimize "On"
